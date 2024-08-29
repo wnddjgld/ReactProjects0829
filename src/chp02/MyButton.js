ce0d5a59f7352cdab5663c268ad625bb362ab672
wnddjgld <wnddjgld@gmail.com>
2024-08-29 16:30:31 +0900

    2장 3장 예제

---
+++ src/chp02/MyButton.js
@@ -0,0 +1,18 @@
+// import ReactDOM from "react-dom/client";
+// import React from "react";
+
+function MyButton(props){
+    const[isClicked, setIsClicked] = React.useState(false)
+
+    return React.createElement(
+        'button',
+        {
+            onClick:() => setIsClicked(true)
+        },
+        isClicked ? 'Clicked' : 'Click here!'
+    )
+}
+
+const domContainer = document.querySelector("#root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(MyButton));
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import  './chp03/Book.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from "./chp03/Library";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
