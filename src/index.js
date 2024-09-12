641d4064c0102140dd6571fbe6d78a4afcd5803d
wnddjgld <wnddjgld@gmail.com>
2024-09-12 16:00:48 +0900

    React0912 실습

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import  './chp03/Book.css'
-import './chp04/Clock.css'
+//import './index.css';
+//import  './chp03/Book.css'
+//import './chp04/Clock.css'
+//import Button from "./chp04/Button.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from "./chp03/Library";
 import ConfirmDialog from "./chp04/ConfirmDialog"
-import Button from "./chp04/Button.css"
 import Clock from "./chp04/Clock";
+import CommentList from './chp05/CommentList'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-    root.render(
-        <React.StrictMode>
-            <Clock/>
-        </React.StrictMode>
-    );
-}, 1000);
+root.render(
+    <React.StrictMode>
+        <CommentList/>
+    </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
